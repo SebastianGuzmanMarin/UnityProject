28908cb33d24efb90d0bf9daa4d41f683d111244
DrYurieZ <sebas_guzman01@hotmail.com>
2020-10-17 17:56:38 -0500

    Creacion de Projecto

---
+++ New Unity Project/Assets/Scripts/AdministradorDeObjetos.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdministradorDeObjetos : MonoBehaviour
+{
+    public GameObject menuAjustes;
+
+    void Start()
+    {
+        menuAjustes.gameObject.SetActive(false);
+        
+    }
+
+    public void OcultarMenuInicial(GameObject menuInicial){
+        menuInicial.gameObject.SetActive(false);
+    }
+    
+    public void AparecerMenuAjustes(GameObject menuAjustes){
+        menuAjustes.gameObject.SetActive(true);
+    }
+
+    public void OcultarMenuAjustes(GameObject menuAjustes){
+
+        menuAjustes.gameObject.SetActive(false);
+    }
+    public void AparecerMenuInicial(GameObject menuInicial){
+
+        menuInicial.gameObject.SetActive(true);
+    }
+
+
+
+
+}
---
+++ New Unity Project/Assets/Scripts/CambiarEscena.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//libreria para usar el cambio de escena
+
+public class CambiarEscena : MonoBehaviour
+{
+    public void CambiarDeEscena(string NombreEscena){
+        SceneManager.LoadScene(NombreEscena);
+    }
+ 
+        
+    
+}
